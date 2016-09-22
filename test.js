ea959d724e1d8a3e2fb5aad1ba2591919a1f07d4
GetGuri <gurvinder.singh@srijan.in>
2016-09-22 15:10:32 +0530

    new changes

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
-var casper = require('casper').create();
 
-casper.start("https://www.google.com",function() {
-  casper.echo("The title of the page is : "+casper.getTitle());
-})
-casper.run();
+// casper.test.begin("first jenkin test",0,function Suite(test){
+//   casper.start("https://www.google.com",function() {
+//     casper.test.assertTitle('Google',"URL matched")
+//     casper.echo("The title of the page is : "+casper.getTitle());
+//   }).run(function(){
+//     casper.test.done();
+//   });
+// });
+casper.test.begin("first jenkin test",0,function Suite(test){
+  casper.start("https://www.google.com",function() {
+    casper.test.assertTitle('Google',"URL matched")
+    casper.echo("The title of the page is : "+casper.getTitle());
+  });
+  casper.run(function(){
+    test.done();
+  })
+});
