544d6574510ab298157aec6bd1f4841b1b9f1187
GetGuri <gurvinder.singh@srijan.in>
2016-09-23 13:08:45 +0530

    third

--- test.js
+++ test.js
@@ -7,11 +7,19 @@
 //     casper.test.done();
 //   });
 // });
+
+
 casper.test.begin("first jenkin test",0,function Suite(test){
-  casper.start("https://www.google.com",function() {
+
+  casper.start("https://www.google.co.in",function() {
+    casper.viewport(1280,1024);
     casper.test.assertTitle('Google',"URL matched")
     casper.echo("The title of the page is : "+casper.getTitle());
+    casper.capture("screenshots/"+Date.now()+".png")
   });
+  casper.then(function(){
+    this.echo("testing firsst jenkins project");
+  })
   casper.run(function(){
     test.done();
   })
